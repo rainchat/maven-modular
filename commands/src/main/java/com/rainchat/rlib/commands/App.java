4251c1967585640b7de6e2c7901aa607884ba2b7
Павел Сидоренко <super.pasha4444@yandex.ru>
2024-09-15 16:42:04 +1000

    1.8

---
+++ commands/src/main/java/com/rainchat/rlib/commands/App.java
@@ -0,0 +1,15 @@
+package com.rainchat.rlib.commands;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static String GREETING = "Hello World!";
+    
+    public static void main( String[] args )
+    {
+        System.out.println( GREETING );
+    }
+}
