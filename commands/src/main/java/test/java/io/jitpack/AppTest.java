6b5e5d2c3bb974ed6723712fe1d1447991e18a22
Павел Сидоренко <super.pasha4444@yandex.ru>
2024-09-15 16:46:21 +1000

    1.8

---
+++ commands/src/main/java/test/java/io/jitpack/AppTest.java
@@ -0,0 +1,17 @@
+package test.java.io.jitpack;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest(String testName )
+    {
+
+    }
+
+}
